537f200b93bd9b58d93db463040b22012e579756
Joseph Rydel <jrydel92@gmail.com>
2025-03-08 20:41:04 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("This is my Jenkins GitHub test that ran on: %s", new Date());
+    }
+}
